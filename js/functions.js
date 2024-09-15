7b7e96e3f108fbf8c4c8f768c35653906d8ef783
joelkarhatsu <joel.karhatsu@gmail.com>
2024-09-15 18:13:13 +0200

    Noppa

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+
+const dice = document.getElementById('dice');
+const diceImage = document.getElementById('diceImage');
+
+
+dice.addEventListener('click', function() {
+    const randomNumber = rollDice(); 
+    diceImage.src = `./img/${randomNumber}.png`; 
+});
